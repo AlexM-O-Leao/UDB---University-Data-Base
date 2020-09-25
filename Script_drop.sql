a40b83276c8e9f1ddc8360e138327c38e06e0dd7
Alexandre Marques <65860878+AlexM-O-Leao@users.noreply.github.com>
2020-09-25 10:23:31 +0100

    Files Upload

---
+++ Script_drop.sql
@@ -0,0 +1,21 @@
+drop TABLE "PROFESSOR" cascade constraints;
+
+drop TABLE "DEPARTAMENTO" cascade constraints;
+
+drop TABLE "ESTUDANTE" cascade constraints;
+
+drop TABLE "PROJETO" cascade constraints;
+
+drop TABLE "PROFESSOR_TRABALHA_PROJETO" cascade constraints;
+
+drop TABLE "PROF_TRABALHA_DEP" cascade constraints;
+
+drop TABLE "PROFESSOR_SUPERVISIONA_REL" cascade constraints;
+
+drop TABLE "ACONSELHA" cascade constraints;
+
+drop TABLE "ESPECIALIDADE" cascade constraints;
+
+drop TABLE "ORGANISMO_FINANCIADOR" cascade constraints;
+
+drop TABLE "TELEFONE" cascade constraints;
